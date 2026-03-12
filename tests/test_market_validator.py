bd090362adc0b0ce6f75050ca6121e719448b56d
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-12 18:41:32 +0100

    Create test_market_validator.py

---
+++ tests/test_market_validator.py
@@ -0,0 +1,9 @@
+import pytest
+
+from market_validator import MarketValidator
+
+
+def test_market_validator_ready():
+    validator = MarketValidator()
+
+    assert validator.is_dutching_ready("MATCH_ODDS") in [True, False]
