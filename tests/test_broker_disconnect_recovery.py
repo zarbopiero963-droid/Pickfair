5bb644be14c5e429d48fced41c23a778b1b28374
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-12 18:48:07 +0100

    Create test_broker_disconnect_recovery.py

---
+++ tests/test_broker_disconnect_recovery.py
@@ -0,0 +1,13 @@
+import pytest
+
+from simulation_broker import SimulationBroker
+
+
+def test_disconnect_recovery():
+    broker = SimulationBroker()
+
+    broker.connected = False
+
+    broker.connect()
+
+    assert broker.connected is True
