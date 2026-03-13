f70324a688e617577469961acea59b245d08d378
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-13 11:21:33 +0100

    Update test_auto_updater.py

--- tests/test_auto_updater.py
+++ tests/test_auto_updater.py
@@ -1,9 +1,18 @@
-import pytest
-
 from auto_updater import AutoUpdater
 
 
-def test_auto_updater_init():
+def test_auto_updater_initial_state():
     updater = AutoUpdater()
 
-    assert updater is not None
+    assert updater.enabled is False
+    assert updater.current_version is None
+
+
+def test_auto_updater_enable_disable():
+    updater = AutoUpdater()
+
+    updater.enable()
+    assert updater.enabled is True
+
+    updater.disable()
+    assert updater.enabled is False
