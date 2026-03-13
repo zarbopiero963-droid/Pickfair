1137d6552ea28ed29e7bc4836714a44fd38d0c0d
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-13 11:22:23 +0100

    Update test_automation_optimizer.py

--- tests/test_automation_optimizer.py
+++ tests/test_automation_optimizer.py
@@ -1,9 +1,19 @@
-import pytest
-
 from automation_optimizer import AutomationOptimizer
 
 
-def test_optimizer_init():
+def test_optimizer_initial_state():
     opt = AutomationOptimizer()
 
-    assert opt is not None
+    assert opt.enabled is True
+    assert isinstance(opt.history, list)
+
+
+def test_optimizer_records_result():
+    opt = AutomationOptimizer()
+
+    opt.record_result(True)
+    opt.record_result(False)
+
+    assert len(opt.history) == 2
+    assert opt.history[0] is True
+    assert opt.history[1] is False
