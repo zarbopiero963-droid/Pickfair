fd67ba8e7e63353889cf1284ca057e012ae94e8e
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-18 17:33:31 +0100

    Delete test_market_tracker_updates.py

--- tests/test_market_tracker_updates.py
+++
@@ -1,9 +0,0 @@
-from market_tracker import MarketTracker
-
-
-def test_market_tracker_updates():
-    tracker = MarketTracker()
-
-    tracker.update_market("1.1", {"price": 2.0})
-
-    assert "1.1" in tracker.markets
