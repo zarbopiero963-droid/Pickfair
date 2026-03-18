cf68fd55b18eec5d9d677c7f4988122042ddc274
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-03-18 11:24:37 +0100

    Create test_latency_profile.py

---
+++ tests/test_latency_profile.py
@@ -0,0 +1,21 @@
+import time
+import statistics
+
+
+def test_latency_profile():
+
+    latencies = []
+
+    for _ in range(100):
+
+        start = time.perf_counter()
+
+        time.sleep(0.002)
+
+        end = time.perf_counter()
+
+        latencies.append(end - start)
+
+    avg_latency = statistics.mean(latencies)
+
+    assert avg_latency < 0.01
